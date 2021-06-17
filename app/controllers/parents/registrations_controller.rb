7c2a1106440c4c3ce9ad94b3164d5e8a9c0a858c
Aniket Bhattacharyea <aniketmail669@gmail.com>
2021-06-17 20:54:10 +0530

    Add profile for parents 2

--- app/controllers/parents/registrations_controller.rb
+++ app/controllers/parents/registrations_controller.rb
@@ -38,9 +38,16 @@ class Parents::RegistrationsController < Devise::RegistrationsController
   # end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+    if resource.persisted? && !params[:bus_registration] && resource.has_current_bus_registration?
+      resource.current_bus_registration.destroy
+    end
+
+    if resource.persisted? && params[:bus_registration] && !resource.has_current_bus_registration?
+      BusService.create(parent: resource, year: Setting.current_school_year_start)
+    end
+  end
 
   # DELETE /resource
   # def destroy
