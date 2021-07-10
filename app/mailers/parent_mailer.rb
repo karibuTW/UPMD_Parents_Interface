8f9cfcb36f875cb4b55af3e0482c8c8ef0e971ae
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2021-07-10 12:29:06 +0530

    Update parent_mailer.rb

--- app/mailers/parent_mailer.rb
+++ app/mailers/parent_mailer.rb
@@ -4,21 +4,21 @@ class ParentMailer < ApplicationMailer
     @token = params[:token]
     @password = params[:password]
     @donated = params[:donated] || false
-    # mail(to: @user.email, subject: 'Please validate your email / Merci de valider votre adresse email / Vui lòng xác nhận email của bạn')
-    mail(to: 'dudumomo+a@gmail.com', subject: 'Please validate your email / Merci de valider votre adresse email / Vui lòng xác nhận email của bạn')
+    mail(to: @user.email, subject: 'Please validate your email / Merci de valider votre adresse email / Vui lòng xác nhận email của bạn')
+    # mail(to: 'dudumomo+a@gmail.com', subject: 'Please validate your email / Merci de valider votre adresse email / Vui lòng xác nhận email của bạn')
   end
 
   def send_donation_mail
     @user = params[:user]
 
-    mail(to: 'dudumomo+a@gmail.com', subject: t('mails.subjects.donation'))
-    # mail(to: @user.email, subject: t('mails.subjects.donation'))
+    #mail(to: 'dudumomo+a@gmail.com', subject: t('mails.subjects.donation'))
+    mail(to: @user.email, subject: t('mails.subjects.donation'))
   end
 
   def send_payment_mail
     @user = params[:user]
 
-    mail(to: 'dudumomo+a@gmail.com', subject: t('mails.subjects.payment_confirmation'))
-    # mail(to: @user.email, subject: t('mails.subjects.payment_confirmation'))
+    #mail(to: 'dudumomo+a@gmail.com', subject: t('mails.subjects.payment_confirmation'))
+    mail(to: @user.email, subject: t('mails.subjects.payment_confirmation'))
   end
 end
