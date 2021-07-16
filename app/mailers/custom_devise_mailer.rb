492ae24f10335c6c92ed39775f8a25adf8f4cab6
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2021-07-16 19:26:27 +0530

    Fix bus driver issue

--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -1,7 +1,7 @@
 class CustomDeviseMailer < Devise::Mailer
   def devise_mail(*args)
     user = args.first
-    I18n.locale = user&.preferred_language || I18n.default_locale
+    I18n.locale = user&.preferred_language || I18n.default_locale if user&.is_a? Parent
     super
   end
 end
