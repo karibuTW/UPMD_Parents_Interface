e1b4a4c3c351436ea2b702db71794cea5f0c295f
Aniket Bhattacharyea <aniket@abhattacharyea.dev>
2021-07-19 17:40:39 +0530

    Skip switch locale in HelloAsso controller

--- app/controllers/helloasso_controller.rb
+++ app/controllers/helloasso_controller.rb
@@ -1,5 +1,5 @@
 class HelloassoController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  skip_before_action :verify_authenticity_token, :switch_locale
 
   def webhook
     begin
