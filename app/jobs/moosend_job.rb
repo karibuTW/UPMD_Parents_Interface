846b1cea1a737f1c4e0a59dad6148882fbde5fff
anonpython1 <abdurrehmansheikh47@gmail.com>
2022-05-08 11:56:33 +0500

    Email present mail condition

--- app/jobs/moosend_job.rb
+++ app/jobs/moosend_job.rb
@@ -9,12 +9,14 @@ class MoosendJob < ApplicationJob
     def add_exisiting_parents_to_mossend
       Parent.all.each do |parent|
         unless parent.moosend_id.present?
-          response = ::Moosend.add_email_to_mailing_list(parent.parent_hash)
-          p '==========================================================================================='
-          p response
-          p '==========================================================================================='
-          unless response["Error"].present?
-            parent.update!(moosend_id: response["Context"]["ID"])
+          if parent.email.present?
+            response = ::Moosend.add_email_to_mailing_list(parent.parent_hash)
+            p '==========================================================================================='
+            p response
+            p '==========================================================================================='
+            unless response["Error"].present?
+              parent.update!(moosend_id: response["Context"]["ID"])
+            end
           end
         end
       end
@@ -24,12 +26,14 @@ class MoosendJob < ApplicationJob
     def add_exisiting_scondary_parents_to_mossend
       SecondaryParent.all.each do |parent|
         unless parent.moosend_id.present?
-          response = ::Moosend.add_email_to_mailing_list(parent.parent_hash)
-          p '==========================================================================================='
-          p response
-          p '==========================================================================================='
-          unless response["Error"].present?
-            parent.update!(moosend_id: response["Context"]["ID"])
+          if parent.email.present?
+            response = ::Moosend.add_email_to_mailing_list(parent.parent_hash)
+            p '==========================================================================================='
+            p response
+            p '==========================================================================================='
+            unless response["Error"].present?
+              parent.update!(moosend_id: response["Context"]["ID"])
+            end
           end
         end
       end
