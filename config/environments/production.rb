5b0f45574b6aaeee8cd75c9a149ad3d41f9df8f8
Aniket <aniket@abhattacharyea.dev>
2022-10-29 22:30:05 +0000

    config park

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,5 +124,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = Rails.application.credentials.dig(:smtp)
   config.action_mailer.default_options = {from: 'contact@upmd.fr'}
-  RAILS_DEFAULT_LOGGER = Logger.new('log/production.log')
+  RAILS_DEFAULT_LOGGER = Logger.new('log/Production.log')
+  logger = Logger.new('log/Production.log')
 end
